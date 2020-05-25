0816f933f681a465abea6840df165b53488b42e6
Tommi Sampo <tommi.sampo@kela.fi>
2020-05-25 15:02:38 +0300

    2.5: erillinen moduuli

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Course from './components/course'
 
 const App = () => {
   const courses = [
@@ -53,57 +54,6 @@ const App = () => {
         <Course key={course.id} course={course} />
       )}
     </>
-  )
-}
-
-const Course = (props) => {
-  return (
-    <>
-      <Header course={props.course} />
-      <Content parts={props.course.parts} />
-      <Total parts={props.course.parts} />
-    </>
-  )
-}
-
-const Header = (props) => {
-  console.log("Headerin propsit")
-  console.log(props)
-  return (
-    <h1>{props.course.name}</h1>
-  )
-}
-
-const Content = ({parts}) => {
-  console.log("Contentin propsit")
-  console.log(parts)
-  return (
-    <>
-      <ul>
-        {parts.map((part, i) =>
-          <Part key={part.id} part={part} />
-        )}
-      </ul>
-    </>
-  )
-}
-
-const Part = (props) => {
-  console.log("Partin propsit")
-  console.log(props)
-  return (
-    <li>
-      {props.part.name} {props.part.exercises}
-    </li>
-  )
-}
-
-const Total = (props) => {
-  console.log("Totalin propsit")
-  console.log(props)
-  const total = props.parts.reduce((sum, part) => sum + part.exercises, 0)
-  return (
-    <p>total of {total} excercises</p>
   )
 }
 
