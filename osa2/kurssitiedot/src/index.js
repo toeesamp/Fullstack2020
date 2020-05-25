053df0deaefe8b77a1aafdce1c16eccf0185e84e
Tommi Sampo <tommi.sampo@kela.fi>
2020-05-25 14:49:37 +0300

    2.4: kurssitiedot step9

--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -2,31 +2,56 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    id: 1,
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
   return (
     <>
-      <Course course={course} />
+      {courses.map((course, i) =>
+        <Course key={course.id} course={course} />
+      )}
     </>
   )
 }
@@ -78,7 +103,7 @@ const Total = (props) => {
   console.log(props)
   const total = props.parts.reduce((sum, part) => sum + part.exercises, 0)
   return (
-    <p>Number of exercises {total}</p>
+    <p>total of {total} excercises</p>
   )
 }
 
