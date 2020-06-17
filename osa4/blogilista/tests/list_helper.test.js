9e521acff2d3f98c11ed9af288933d92c4d9e400
toeesamp <tommi.sampo@gmail.com>
2020-06-17 17:10:44 +0300

    4.7 testing

--- osa4/blogilista/tests/list_helper.test.js
+++ osa4/blogilista/tests/list_helper.test.js
@@ -58,6 +58,57 @@ const blogs = [
     }
 ]
 
+const blogsInverted = [
+    {
+        _id: '5a422b891b54a676234d17fa',
+        title: 'First class tests',
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+        likes: 10,
+        __v: 0
+    },
+    {
+        _id: '5a422ba71b54a676234d17fb',
+        title: 'TDD harms architecture',
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+        likes: 0,
+        __v: 0
+    },
+    {
+        _id: '5a422bc61b54a676234d17fc',
+        title: 'Type wars',
+        author: 'Robert C. Martin',
+        url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+        likes: 2,
+        __v: 0
+    },
+    {
+        _id: '5a422a851b54a676234d17f7',
+        title: 'React patterns',
+        author: 'Michael Chan',
+        url: 'https://reactpatterns.com/',
+        likes: 7,
+        __v: 0
+    },
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 5,
+        __v: 0
+    },
+    {
+        _id: '5a422b3a1b54a676234d17f9',
+        title: 'Canonical string reduction',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+        likes: 12,
+        __v: 0
+    }
+]
+
 const oneBlog = []
 oneBlog.push( blogs[0] )
 
@@ -116,13 +167,46 @@ describe('most blogs', () => {
                 blogs: 1
             })
     })
-
     test('Robert C. Martin should have 3 blogs', () => {
         const result = listHelper.mostBlogs(blogs)
         expect(result).toEqual(
             {
                 author: 'Robert C. Martin',
                 blogs: 3
+            })
+    })
+    test('invert should not have an effect', () => {
+        const result = listHelper.mostBlogs(blogsInverted)
+        expect(result).toEqual(
+            {
+                author: 'Robert C. Martin',
+                blogs: 3
+            })
+    })
+})
+
+describe('most likes', () => {
+    test('most likes of empty list should be empty object', () => {
+        const result =listHelper.mostLikes(emptyList)
+        expect(result).toEqual({})
+    })
+    test('most likes of single blog should be the the authors name and the likes of the blog', () => {
+        const result = listHelper.mostLikes(oneBlog)
+        expect(result).toEqual(
+            {
+                author: 'Michael Chan',
+                likes: 7
+            })
+    })
+    //#############################################
+    //FIXME remove only
+    //#############################################
+    test('Edsger W. Dijkstra should have 17 likes', () => {
+        const result = listHelper.mostLikes(blogs)
+        expect(result).toEqual(
+            {
+                author: 'Edsger W. Dijkstra',
+                likes: 17
             })
     })
 })
--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,5 +1,6 @@
 const _ = require('lodash')
 
+
 const dummy = (blogs) => {
     return 1
 }
@@ -29,13 +30,14 @@ const mostBlogs = (blogs) => {
     //discard everything but the author name
     const authors = blogs.map(({ author }) => author)
 
+
     const resultArray =_(authors)
         //count the occurrence of author names
         .countBy()
         //convert to array of [author, count] pairs for maxBy
         .toPairs()
-        //get the pair with largest count
-        .maxBy()
+        //get the pair with largest count (last element in the tuple)
+        .maxBy(_.last)
 
     const resultObject = {
         'author': resultArray[0],
@@ -44,6 +46,87 @@ const mostBlogs = (blogs) => {
     return resultObject
 }
 
+const mostLikes = (blogs) => {
+    if (blogs.length === 0) {
+        return {}
+    }
+
+    //const authors = blogs.map(({ author }) => author)
+    //console.log(authors)
+    //const uniqueAuthors = _.uniq(authors)
+    //console.log('unique authors \n',uniqueAuthors)
+
+
+
+
+    //const authorsAndLikes = blogs.map(({ author, likes }) => [author, likes])
+    //const obj = Object.fromEntries(authorsAndLikes)
+    //console.log(obj)
+    //console.log('authors and likes \n',authorsAndLikes)
+
+    //const authorsAndLikesObject = _.fromPairs(authorsAndLikes)
+    //console.log('authors and likes object \n',authorsAndLikesObject)
+
+
+    const test = blogs.map(({ author, likes }) => ({ author, likes }))
+    console.log(test)
+
+
+    //const uniqueAuthorsWithLikes = _.uniq(authorsAndLikes, 'author')
+    //console.log('unique authors with likes\n',uniqueAuthorsWithLikes)
+
+
+    const test2 = _.groupBy(test, 'author')
+    console.log(test2)
+
+    //const test3 = _.mapValues(test2, blogsByAuthor => _.sum(blogsByAuthor))
+    //const test3 = _.mapValues(test2, blogsByAuthor => blogsByAuthor.map(_.toPairs(blogsByAuthor)))
+
+    //#############
+    //tää on se oikee
+    //const test3 = _.mapValues(test2, blogsByAuthor => {
+    //    console.log('new array', blogsByAuthor)
+    //    const sum = _.sumBy(blogsByAuthor, 'likes')
+    //    console.log(sum)
+    //    //blogsByAuthor.map(a => console.log(a))
+    //})
+    const test3 = _.mapValues(test2, blogsByAuthor => _.sumBy(blogsByAuthor, 'likes'))
+
+    console.log('test3',test3)
+
+    const test4 = _(test3).toPairs().value()
+    console.log('test4', test4)
+
+    const final = _(test3).toPairs().maxBy(_.last)
+    console.log('final', final)
+
+    //var objects = [
+    //    { 'n': 4 },
+    //    { 'n': 6 }
+    //]
+
+    //var testsum = _.sum(objects, 'n')
+    //console.log('testsum', testsum)
+
+    //var testsum2 = _.sumBy(objects, function(object) {
+    //    return object.n
+    //})
+    //console.log('testsum2', testsum2)
+
+    //console.log(test3)
+
+    //const test3 = _.groupBy(authorsAndLikes)
+    //console.log(test3)
+    //split attribute names
+    //sum
+    //sama kun most blogs
+    const resultObject = {
+        'author': final[0],
+        'likes': final[1]
+    }
+    return resultObject
+}
+
 module.exports = {
-    dummy, totalLikes, favoriteBlog, mostBlogs
+    dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes
 }
