9ca156381c2df38b55dc88503b62aa083467a349
toeesamp <tommi.sampo@gmail.com>
2020-06-29 15:07:32 +0300

    4.10: blogilistan testit, step3

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -4,10 +4,7 @@ const helper = require('./test_helper')
 const app = require('../app')
 const Blog = require('../models/blog')
 const { initialBlogs } = require('./test_helper')
-
 const api = supertest(app)
-
-
 
 beforeEach(async () => {
     await Blog.deleteMany({})
@@ -31,6 +28,27 @@ test('_id is converted to id', async () => {
     response.body.map(blog => expect(blog.id).toBeDefined())
 })
 
+test('a valid blog can be added ', async () => {
+    const newBlog = {
+        title: 'test3',
+        author: 'testauthor3',
+        url: 'testurl3',
+        likes: 3
+    }
+
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+
+    const contents = response.body.map(r => r.title)
+
+    expect(response.body).toHaveLength(initialBlogs.length + 1)
+    expect(contents).toContain('test3')
+})
 
 afterAll(() => {
     mongoose.connection.close()
