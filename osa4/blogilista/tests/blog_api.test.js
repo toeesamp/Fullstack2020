6a868d8433026aa0f3a4b5b227d83081d040c432
toeesamp <tommi.sampo@gmail.com>
2020-06-29 14:31:06 +0300

    4.8: blogilistan testit, step 1

---
+++ osa4/blogilista/tests/blog_api.test.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const helper = require('./test_helper')
+const app = require('../app')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+
+    let blogObject = new Blog(helper.initialBlogs[0])
+    await blogObject.save()
+
+    blogObject = new Blog(helper.initialBlogs[1])
+    await blogObject.save()
+})
+
+test('blogs are returned as json', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+})
+
+test('there are two blogs', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+})
+
+afterAll(() => {
+    mongoose.connection.close()
+})
