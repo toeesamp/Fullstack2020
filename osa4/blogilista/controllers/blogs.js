ca6b8e25980a960ffc6588bb853d2afa32a55440
toeesamp <tommi.sampo@gmail.com>
2020-07-01 13:54:58 +0300

    4.20*: blogilistan laajennus, step9

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -3,13 +3,13 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
-const getTokenFrom = request => {
-    const authorization = request.get('authorization')
-    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-        return authorization.substring(7)
-    }
-    return null
-}
+//const getTokenFrom = request => {
+//    const authorization = request.get('authorization')
+//    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+//        return authorization.substring(7)
+//    }
+//    return null
+//}
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog
@@ -20,16 +20,10 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
-    const token = getTokenFrom(request)
+    //const token = getTokenFrom(request)
 
-    //to be fixed in 4.19
-    //const user = await User.findById(body.userId)
-    //const users = await User.find({})
-    //assumes that at least one user exists
-    //const user = users[0]
-
-    const decodedToken = jwt.verify(token, process.env.SECRET)
-    if (!token || !decodedToken.id) {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!request.token || !decodedToken.id) {
         return response.status(401).json({ error: 'token missing or invalid' })
     }
     const user = await User.findById(decodedToken.id)
