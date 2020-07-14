f04b36f0a625a8d1503139f764ce47538c7d742a
toeesamp <tommi.sampo@gmail.com>
2020-07-14 11:34:20 +0300

    5.18: blogilistan end to end -testit, step2

--- osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,6 +1,13 @@
 describe('Blog app', function () {
     beforeEach(function () {
         cy.request('POST', 'http://localhost:3001/api/testing/reset')
+        cy.request('POST', 'http://localhost:3001/api/testing/reset')
+        const user = {
+            name: 'test boi',
+            username: 'test',
+            password: 'test'
+        }
+        cy.request('POST', 'http://localhost:3001/api/users/', user)
         cy.visit('http://localhost:3000')
     })
 
@@ -9,5 +16,27 @@ describe('Blog app', function () {
         cy.get('#username')
         cy.get('#password')
         cy.get('#login-button')
+    })
+
+    describe('Login', function () {
+        it('succeeds with correct credentials', function () {
+            cy.get('#username').type('test')
+            cy.get('#password').type('test')
+            cy.get('#login-button').click()
+
+            cy.get('.info').contains('succesfully logged in')
+            cy.contains('Logged in as test')
+
+        })
+
+        it('fails with wrong credentials', function () {
+            cy.get('#username').type('test')
+            cy.get('#password').type('asd')
+            cy.get('#login-button').click()
+
+            cy.get('.error').should('contain', 'wrong credentials')
+                .and('have.css', 'color', 'rgb(255, 0, 0)')
+                .and('have.css', 'border-style', 'solid')
+        })
     })
 })
