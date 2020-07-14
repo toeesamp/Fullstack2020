d2c3b375924abf775ede2d2ca139f5c7df6a403c
toeesamp <tommi.sampo@gmail.com>
2020-07-14 13:36:52 +0300

    5.22: blogilistan end to end -testit, step6

--- osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,6 +1,6 @@
 describe('Blog app', function () {
     beforeEach(function () {
-        cy.request('POST', 'http://localhost:3001/api/testing/reset')
+        //cy.request('POST', 'http://localhost:3001/api/testing/reset')
         cy.request('POST', 'http://localhost:3001/api/testing/reset')
         const user = {
             name: 'test boi',
@@ -42,12 +42,7 @@ describe('Blog app', function () {
 
     describe('When logged in', function () {
         beforeEach(function () {
-            cy.get('#username').type('test')
-            cy.get('#password').type('test')
-            cy.get('#login-button').click()
-
-            cy.get('.info').contains('succesfully logged in')
-            cy.contains('Logged in as test')
+            cy.login({ username: 'test', password: 'test' })
         })
 
         it('A blog can be created', function () {
@@ -61,29 +56,82 @@ describe('Blog app', function () {
             cy.contains('testauthor')
         })
 
-        it('A blog can be liked', function () {
-            cy.contains('new blog').click()
-            cy.get('#title').type('testtitle')
-            cy.get('#author').type('testauthor')
-            cy.get('#url').type('testurl')
-            cy.get('#create-blog-button').click()
+        describe('and a blog exists', () => {
+            beforeEach(function () {
+                cy.createBlog({
+                    title: 'testtitle',
+                    author: 'testauthor',
+                    url: 'testurl'
+                })
+            })
 
-            cy.contains('testtitle testauthor')
-                .contains('view')
-                .click()
+            it('A blog can be liked', function () {
+                cy.contains('testtitle testauthor')
+                    .contains('view')
+                    .click()
 
-            cy.contains('testtitle testauthor')
-                .contains('likes: 0')
+                cy.contains('testtitle testauthor')
+                    .contains('likes: 0')
 
+                cy.contains('testtitle testauthor')
+                    .contains('like')
+                    .click()
+                    .click()
 
+                cy.contains('testtitle testauthor')
+                    .contains('likes: 2')
+            })
 
-            cy.contains('testtitle testauthor')
-                .contains('like')
-                .click()
+            it('A blog can be deleted', function () {
+                cy.contains('testtitle testauthor')
+                    .contains('view')
+                    .click()
 
-            cy.contains('testtitle testauthor')
-                .contains('likes: 1')
+                cy.contains('testtitle testauthor')
+                    .contains('delete')
+                    .click()
 
+                cy.get('html').should('not.contain', 'testtitle testauthor')
+            })
+
+            it('blogs are listed in the order of likes', function () {
+                cy.createBlog({
+                    title: 'testtitle2',
+                    author: 'testauthor2',
+                    url: 'testurl2'
+                })
+                cy.createBlog({
+                    title: 'testtitle3',
+                    author: 'testauthor3',
+                    url: 'testurl3'
+                })
+
+                cy.contains('testtitle3 testauthor3')
+                    .contains('view')
+                    .click()
+
+                cy.contains('testtitle3 testauthor3')
+                    .contains('like')
+                    .click()
+                    .click()
+
+                cy.contains('testtitle2 testauthor2')
+                    .contains('view')
+                    .click()
+
+                cy.contains('testtitle2 testauthor2')
+                    .contains('like')
+                    .click()
+                    .click()
+                    .click()
+
+                cy.get('.blog').should(($blogs) => {
+                    expect($blogs).to.have.length(3)
+                    expect($blogs.eq(0)).to.contain('testtitle2 testauthor2')
+                    expect($blogs.eq(1)).to.contain('testtitle3 testauthor3')
+                    expect($blogs.eq(2)).to.contain('testtitle testauthor')
+                })
+            })
         })
     })
 
