62fcaf88087b062954ff91b91ff5bab3b7baa14a
toeesamp <tommi.sampo@gmail.com>
2020-07-15 10:51:50 +0300

    6.9 anekdootit, step7

--- osa6/redux-anecdotes/src/components/AnecdoteList.js
+++ osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -18,7 +18,7 @@ const Anecdote = ({ anecdote, handleClick }) => {
 
 const AnecdoteList = () => {
     const dispatch = useDispatch()
-    const anecdotes = useSelector(state => state)
+    const anecdotes = useSelector(state => state.anecdotes)
 
     return(
         <div>
