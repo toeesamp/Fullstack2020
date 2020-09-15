1c1ce020e9f079a0c278c4e736bd00ce31277002
toeesamp <tommi.sampo@gmail.com>
2020-09-15 12:11:11 +0300

    9.17: patientor, step2

--- osa9/patientor-backend/src/index.ts
+++ osa9/patientor-backend/src/index.ts
@@ -10,7 +10,7 @@ app.use(cors());
 const PORT = 3001;
 
 app.get('/api/ping', (_req, res) => {
-    console.log('someone pinged here');
+    //console.log('someone pinged here');
     res.send('pong');
 });
 
