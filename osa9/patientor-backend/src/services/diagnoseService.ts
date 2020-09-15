3bed2f01de6362fc259d6a907ccac5fddb1af8a6
toeesamp <tommi.sampo@gmail.com>
2020-09-15 22:30:00 +0300

    9.19: patientor, step4

--- osa9/patientor-backend/src/services/diagnoseService.ts
+++ osa9/patientor-backend/src/services/diagnoseService.ts
@@ -1,10 +1,10 @@
 import diagnoseData from '../../data/diagnoses.json';
 
-import { Diagnose } from '../types';
+import { Diagnosis } from '../types';
 
-const diagnoses: Array<Diagnose> = diagnoseData as Array<Diagnose>;
+const diagnoses: Array<Diagnosis> = diagnoseData as Array<Diagnosis>;
 
-const getDiagnoses = (): Array<Diagnose> => {
+const getDiagnoses = (): Array<Diagnosis> => {
   return diagnoses;
 };
 
--- osa9/patientor-backend/src/types.ts
+++ osa9/patientor-backend/src/types.ts
@@ -1,12 +1,54 @@
-export interface Diagnose {
+export interface Diagnosis {
     code: string;
     name: string;
     latin?: string;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
+interface BaseEntry {
+    id: string;
+    description: string;
+    date: string;
+    specialist: string;
+    diagnosisCodes?: Array<Diagnosis['code']>;
 }
+
+export enum HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+    type: "HealthCheck";
+    healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+    type: "OccupationalHealthcare";
+    employerName: string;
+    sickLeave?: {
+        startDate: string;
+        endDate: string;
+    }
+}
+
+interface HospitalEntry extends BaseEntry {
+    type: "Hospital";
+    discharge: {
+        date: string;
+        criteria: string;
+    }
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+//export interface Entry {
+//}
 export interface Patient {
     id: string;
     name: string;
@@ -17,7 +59,7 @@ export interface Patient {
     entries: Entry[];
 }
 
-export type NonSensitivePatientData = Omit<Patient, 'ssn' | 'entries' >;
+export type NonSensitivePatientData = Omit<Patient, 'ssn' | 'entries'>;
 
 export type NewPatient = Omit<Patient, 'id'>;
 
